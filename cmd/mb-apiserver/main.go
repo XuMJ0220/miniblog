e5b51de7608d185e8e12a6deb09f358b6cac12a1
xumingjie <1044011439@qq.com>
2025-12-09 01:03:49 +0800

    feat: 初始化项目(Air LICENSE Makefile)

--- cmd/mb-apiserver/main.go
+++ cmd/mb-apiserver/main.go
@@ -1,7 +1,11 @@
+// Copyright 2024 许铭杰 (1044011439@qq.com). All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("hello world")
 }
